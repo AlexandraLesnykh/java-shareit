b8fbb5b3f43bae5f95208e13b28993df7ae9dcc3
Alexandra <shuro4kaln7@mail.ru>
2023-05-08 19:54:30 +0000

    Поправдено оформление 3

--- server/src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -14,7 +14,6 @@ import ru.practicum.shareit.exeptions.BadRequestException;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
-import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.UserService;
 
 import java.time.LocalDateTime;
