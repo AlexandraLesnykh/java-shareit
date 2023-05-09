21348755c74d7079f1aa156659f9b117fd5e587d
Alexandra <shuro4kaln7@mail.ru>
2023-05-09 21:29:26 +0000

    Изменениеобработчика ошибок

--- gateway/src/main/java/ru/practicum/shareit/exeptions/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exeptions/ErrorHandler.java
@@ -1,20 +1,43 @@
 package ru.practicum.shareit.exeptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.validator.ValidationErrorResponse;
+import ru.practicum.shareit.validator.Violation;
 
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
-    public ResponseEntity<String> processUnmergeException(final MethodArgumentNotValidException e) {
-        return new ResponseEntity<>("Unknown state: UNSUPPORTED_STATUS", HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        List<Violation> list = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(list);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onConstraintViolationException(ConstraintViolationException e) {
+        List<Violation> list = e.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(list);
     }
 
 }
