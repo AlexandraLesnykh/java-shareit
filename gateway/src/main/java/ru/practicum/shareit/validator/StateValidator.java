6c2d0249b8fa5ff47dcea3c8d5f61f8c85aa6cb9
Alexandra <shuro4kaln7@mail.ru>
2023-05-10 09:37:59 +0000

    Правка валидации 8

--- gateway/src/main/java/ru/practicum/shareit/validator/StateValidator.java
+++ gateway/src/main/java/ru/practicum/shareit/validator/StateValidator.java
@@ -6,10 +6,12 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StateValidator implements ConstraintValidator<StateValidation, String> {
     public boolean isValid(String stateName, ConstraintValidatorContext cxt) {
-        List list = Arrays.asList("ALL", "CURRENT", "FUTURE", "PAST", "WAITING", "REJECTED", BookingState.from(stateName));
-        return list.contains(stateName);
+        List list = Arrays.asList("ALL", "CURRENT", "FUTURE", "PAST", "WAITING", "REJECTED");
+        Optional<BookingState> state = BookingState.from(stateName);
+        return list.contains(stateName) || state.isEmpty();
     }
 }
