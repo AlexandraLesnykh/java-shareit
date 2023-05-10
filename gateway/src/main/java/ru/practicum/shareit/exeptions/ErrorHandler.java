733bdd0addf4927355eda49a43945900202c5187
Alexandra <shuro4kaln7@mail.ru>
2023-05-10 12:06:17 +0000

    Правка хендлера3

--- gateway/src/main/java/ru/practicum/shareit/exeptions/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exeptions/ErrorHandler.java
@@ -1,16 +1,11 @@
 package ru.practicum.shareit.exeptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.Set;
 
 @RestControllerAdvice
 public class ErrorHandler {
