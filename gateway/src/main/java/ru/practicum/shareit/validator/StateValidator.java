ebbdba241ba69deaeabc7253950210045a6b8eb6
Alexandra <shuro4kaln7@mail.ru>
2023-05-10 12:42:17 +0000

    Исправлены booking клиент и контроллер 2

--- gateway/src/main/java/ru/practicum/shareit/validator/StateValidator.java
+++ gateway/src/main/java/ru/practicum/shareit/validator/StateValidator.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.validator;
 
+import ru.practicum.shareit.booking.dto.BookingState;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
@@ -7,7 +9,11 @@ import java.util.List;
 
 public class StateValidator implements ConstraintValidator<StateValidation, String> {
     public boolean isValid(String stateName, ConstraintValidatorContext cxt) {
-        List list = Arrays.asList("ALL", "CURRENT", "FUTURE", "PAST", "WAITING", "REJECTED");
+        //    List list = Arrays.asList("ALL", "CURRENT", "FUTURE", "PAST", "WAITING", "REJECTED");
+        List list = Arrays.asList(BookingState.from(stateName));
+        if (!list.contains(stateName)) {
+            throw new IllegalArgumentException("Unknown state" + stateName);
+        }
         return list.contains(stateName);
     }
 }
