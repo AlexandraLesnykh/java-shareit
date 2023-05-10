1f5502ee34ba613f202d8feea46f981c9323b453
Alexandra <shuro4kaln7@mail.ru>
2023-05-10 08:18:56 +0000

    Правка

--- gateway/src/main/java/ru/practicum/shareit/validator/ValidationErrorResponse.java
+++ gateway/src/main/java/ru/practicum/shareit/validator/ValidationErrorResponse.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.validator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @RequiredArgsConstructor
 public class ValidationErrorResponse {
